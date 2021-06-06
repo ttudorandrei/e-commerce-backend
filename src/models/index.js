ba3e39ce00b5ede5f65e1e3b3c901eaa115acce6
Tudor Tocan <tudor.tocan@gmail.com>
2021-06-06 14:33:41 +0100

    refactor

--- src/models/index.js
+++ src/models/index.js
@@ -20,16 +20,14 @@ Category.hasMany(Product, {
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: {
-    model: ProductTag,
-  },
+  through: ProductTag,
+  foreignKey: "product_id",
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: {
-    model: ProductTag,
-  },
+  through: ProductTag,
+  foreignKey: "tag_id",
 });
 
 module.exports = {
